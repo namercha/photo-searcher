d7beb6c426b4b6862008a11fc60b5792ec7ca11b
nabil <merchant.nabil@gmail.com>
2022-02-03 21:42:11 -0600

    Splitting up into smaller functions.

--- main.py
+++ main.py
@@ -9,27 +9,29 @@ Builder.load_file('frontend.kv')
 
 class FirstScreen(Screen):
 
-    def search_image(self):
+    def get_image_link(self):
         # Get the user query from the text input box
         query = self.manager.current_screen.ids.user_query.text
         print(query)
-
         # Get the wikipedia page and extract list of image links
         page = wikipedia.page(query)
         # Get the first image returned
         image = page.images[0]
+        return image
 
+    def download_image(self):
         # Download the image
         headers = {'User-agent': 'Mozilla/5.0'}
-        response = requests.get(image, headers=headers)
-
+        response = requests.get(self.get_image_link(), headers=headers)
         # Write the returned image into a file locally
         image_path = 'files/image.jpg'
         with open(image_path, 'wb') as file:
             file.write(response.content)
+        return image_path
 
+    def set_image(self):
         # Set the downloaded image in the image widget
-        self.manager.current_screen.ids.img.source = image_path
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 
 class RootWidget(ScreenManager):
