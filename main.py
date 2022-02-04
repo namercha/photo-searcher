fd4f3a6a29a2a2ac58720ae3da374c18a28c6c23
nabil <merchant.nabil@gmail.com>
2022-02-03 21:38:34 -0600

    Adding proper functionality with the screen and search.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
 
 Builder.load_file('frontend.kv')
 
@@ -8,7 +10,26 @@ Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
 
     def search_image(self):
-        pass
+        # Get the user query from the text input box
+        query = self.manager.current_screen.ids.user_query.text
+        print(query)
+
+        # Get the wikipedia page and extract list of image links
+        page = wikipedia.page(query)
+        # Get the first image returned
+        image = page.images[0]
+
+        # Download the image
+        headers = {'User-agent': 'Mozilla/5.0'}
+        response = requests.get(image, headers=headers)
+
+        # Write the returned image into a file locally
+        image_path = 'files/image.jpg'
+        with open(image_path, 'wb') as file:
+            file.write(response.content)
+
+        # Set the downloaded image in the image widget
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
